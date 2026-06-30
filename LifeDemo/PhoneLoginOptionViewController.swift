886324dd9ee70274777666c6ad416aa57cfcfb5d
Nozanin A <an13119@auca.kg>
2026-06-30 10:13:56 +0500

    Update text design

--- LifeDemo/PhoneLoginOptionViewController.swift
+++ LifeDemo/PhoneLoginOptionViewController.swift
@@ -13,17 +13,22 @@ class PhoneLoginOptionViewController: UIViewController {
     
     private let tabLabel : UILabel = {
         let label = UILabel()
-        label.text = "Мобильная связь"
+        label.text = "Введите номер телефона"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .red
         return label
     }()
     
     private func applyConstraints() {
         [
-            tabLabel.heightAnchor.constraint(equalToConstant: 128),
-            tabLabel.widthAnchor.constraint(equalToConstant: 128),
             tabLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
-            tabLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tabLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            tabLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            tabLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ].forEach { $0.isActive = true }
     }
     
